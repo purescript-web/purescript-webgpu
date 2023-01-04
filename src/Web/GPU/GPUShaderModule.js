f80854ffc3edf959f0b3e68a8c05c77a53024ef1
Mike Solomon <mike@meeshkan.com>
2023-01-04 16:50:09 +0200

    adds gpushadermodule

---
+++ src/Web/GPU/GPUShaderModule.js
@@ -0,0 +1,1 @@
+export const compilationInfoImpl = (shaderModule) => () => shaderModule.compilationInfo();
---
+++ src/Web/GPU/GPUShaderModule.purs
@@ -0,0 +1,41 @@
+module Web.GPU.GPUShaderModule (compilationInfo, GPUCompilationMessageType, error, warning, info) where
+
+import Prelude
+
+import Effect (Effect)
+import Web.GPU.Internal.Types (GPUShaderModule)
+import Web.GPU.Internal.Unsigned (UnsignedLongLong)
+import Web.Promise (Promise)
+
+newtype GPUCompilationMessageType = GPUCompilationMessageType String
+
+derive instance Eq GPUCompilationMessageType
+derive instance Ord GPUCompilationMessageType
+derive newtype instance Show GPUCompilationMessageType
+
+error :: GPUCompilationMessageType
+error = GPUCompilationMessageType "error"
+
+warning :: GPUCompilationMessageType
+warning = GPUCompilationMessageType "warning"
+
+info :: GPUCompilationMessageType
+info = GPUCompilationMessageType "info"
+
+type GPUCompilationMessage =
+  { message :: String
+  , type :: GPUCompilationMessageType
+  , lineNum :: UnsignedLongLong
+  , linePos :: UnsignedLongLong
+  , offset :: UnsignedLongLong
+  , length :: UnsignedLongLong
+  }
+
+type GPUCompilationInfo =
+  { messages :: Array GPUCompilationMessage
+  }
+
+foreign import compilationInfoImpl :: GPUShaderModule -> Effect (Promise GPUCompilationInfo)
+
+compilationInfo :: GPUShaderModule -> Effect (Promise GPUCompilationInfo)
+compilationInfo = compilationInfoImpl

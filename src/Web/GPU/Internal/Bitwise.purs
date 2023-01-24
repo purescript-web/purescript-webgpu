49d92001de38de4f49dfe6289203a9957b7836de
Mike Solomon <mike@meeshkan.com>
2023-01-24 08:53:38 +0200

    Adds bitwise for int and uint

--- src/Web/GPU/Internal/Bitwise.purs
+++ src/Web/GPU/Internal/Bitwise.purs
@@ -1,6 +1,16 @@
 module Web.GPU.Internal.Bitwise (class Bitwise, or, (.|.)) where
 
+import Data.Int.Bits as IntBits
+import Data.UInt (UInt)
+import Data.UInt as UIntBits
+
 class Bitwise a where
   or :: a -> a -> a
+
+instance Bitwise Int where
+  or = IntBits.or
+
+instance Bitwise UInt where
+  or = UIntBits.or
 
 infixl 10 or as .|.
